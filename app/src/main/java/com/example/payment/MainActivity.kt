9cfae37d06a03f2ae00fbce6916e8d0cae88cab4
Erkhembayar <erkhembayar021@gmail.com>
2022-01-18 16:29:33 +0800

    Initial commit

---
+++ app/src/main/java/com/example/payment/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.payment
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import com.example.payment.ui.theme.AndroidLibraryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidLibraryTheme {
+                Box(modifier = Modifier.fillMaxSize()) {
+
+                }
+            }
+        }
+    }
+}
