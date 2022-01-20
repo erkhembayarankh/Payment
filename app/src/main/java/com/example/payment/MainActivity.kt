538f4ee9fcfccac425aa6a8238dc1ca13dd49471
Erkhembayar <erkhembayar021@gmail.com>
2022-01-20 14:22:08 +0800

    wip

--- app/src/main/java/com/example/payment/MainActivity.kt
+++ app/src/main/java/com/example/payment/MainActivity.kt
@@ -1,21 +1,48 @@
 package com.example.payment
+
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.payment.ui.theme.AndroidLibraryTheme
+import com.example.testsdk.ImagePreview
+import com.example.testsdk.ListPaymentType
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AndroidLibraryTheme {
-                Box(modifier = Modifier.fillMaxSize()) {
-
-                }
-            }
+            val list = mutableListOf<String>("TDB","Golomt")
+            ListPaymentType(paymentTypeList = list, painter = painterResource(id = R.drawable.tdbm))
         }
     }
 }
+
+
